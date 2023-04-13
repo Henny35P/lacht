e506ce4729fd16e9d056b966d0fbe22c8bbf9112
Hans Villarroel <hans.villarroel@usach.cl>
2023-04-12 22:34:11 -0400

    Fixed type error on author.username

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -36,7 +36,10 @@ export const postsRouter = createTRPCRouter({
 
       return {
         post,
-        author,
+        author:{
+            ...author,
+            username: author.username
+        },
       };
     });
   }),
